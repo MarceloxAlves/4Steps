765f413769d9fc93fe63406adc84fa58f8a46a80
Marcelo <marceloalvessoft@gmail.com>
2019-06-19 22:07:59 -0300

    Firebase Adicionado

---
+++ src/boot/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: 'AIzaSyCYBXkapDOuWzz_MqwOdxKBaA9SdLoDyvs',
+  authDomain: 'steps-d0a6f.firebaseapp.com',
+  databaseURL: 'https://steps-d0a6f.firebaseio.com',
+  projectId: 'steps-d0a6f',
+  storageBucket: '',
+  messagingSenderId: '656296469539',
+  appId: '1:656296469539:web:7b6788c8ccd1e05a'
+}
+
+const fireApp = firebase.initializeApp(firebaseConfig)
+
+export const AUTH = fireApp.auth()
+
+export default ({ app, router, Vue }) => {
+  Vue.prototype.$auth = AUTH
+}
